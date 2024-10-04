db0b17dacdc8c449523cd46e1c47fc3a967126f9
Alexandre <c.alexandre@deepidoo.com>
2024-10-04 14:24:06 +0200

    feat: Add documentation in code

--- lib/now_playing.dart
+++ lib/now_playing.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/services.dart';
 
+/// NowPlaying is a Flutter plugin that provides easy integration with iOS Now Playing controls and information display.
+/// This package allows you to update the Now Playing information and handle remote control events for media playback.
 class NowPlaying {
   static final NowPlaying instance = NowPlaying._internal();
   static MethodChannel? _channel;
   bool _isInitialized = false;
 
+  /// Private constructor for the NowPlaying class.
   NowPlaying._internal();
 
-  Future<void> updateNowPlayingInfo({
-    required String title,
-    required String artist,
-    required double duration,
-    required double currentTime,
-    required double playbackRate,
-    required String imageUrl,
-  }) async {
-    if (!_isInitialized) {
-      throw Exception(
-          'NowPlayable is not initialized. Call initialize() first.');
-    }
-    await _channel!.invokeMethod('updateNowPlayingInfo', {
-      'title': title,
-      'artist': artist,
-      'duration': duration,
-      'currentTime': currentTime,
-      'playbackRate': playbackRate,
-      'imageUrl': imageUrl,
-    });
-  }
-
+  /// Initialize the NowPlaying plugin with the necessary event handlers.
+  ///
+  /// This method sets up the remote command handlers for media playback events.
+  /// It also sets up the method call handler to handle events from the native iOS Now Playing plugin.
+  ///
+  /// Parameters:
+  /// - [onPlay] : A function to handle the play event.
+  /// - [onPause] : A function to handle the pause event.
+  /// - [onNextTrack] : A function to handle the next track event.
+  /// - [onPreviousTrack] : A function to handle the previous track event.
+  /// - [onSeek] : A function to handle the seek event.
   Future<void> initialize({
     required Function onPlay,
     required Function onPause,
@@ -61,5 +53,38 @@ class NowPlaying {
     });
 
     _isInitialized = true;
+  }
+
+  /// Update the Now Playing information.
+  ///
+  /// This method updates the Now Playing information in the iOS Now Playing plugin.
+  ///
+  /// Parameters:
+  /// - [title]   : The title of the current track.
+  /// - [artist] : The artist of the current track.
+  /// - [duration] : The duration of the current track in seconds.
+  /// - [currentTime] : The current playback time of the track in seconds.
+  /// - [playbackRate] : The playback rate of the track.
+  /// - [imageUrl] : The URL of the album art image.
+  Future<void> updateNowPlayingInfo({
+    required String title,
+    required String artist,
+    required double duration,
+    required double currentTime,
+    required double playbackRate,
+    required String imageUrl,
+  }) async {
+    if (!_isInitialized) {
+      throw Exception(
+          'NowPlayable is not initialized. Call initialize() first.');
+    }
+    await _channel!.invokeMethod('updateNowPlayingInfo', {
+      'title': title,
+      'artist': artist,
+      'duration': duration,
+      'currentTime': currentTime,
+      'playbackRate': playbackRate,
+      'imageUrl': imageUrl,
+    });
   }
 }
